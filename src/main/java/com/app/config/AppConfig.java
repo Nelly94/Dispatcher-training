13b42a4b32f70aa6e8a6d7a20d91b01e5b23ce00
nellyiriberri <nellyiriberri@gmail.com>
2020-06-11 11:25:14 +0200

    Create messages properties for labels internationalitation

--- src/main/java/com/app/config/AppConfig.java
+++ src/main/java/com/app/config/AppConfig.java
@@ -1,9 +1,20 @@
 package com.app.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 @Import({WebConfig.class, JpaConfig.class})
 public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
